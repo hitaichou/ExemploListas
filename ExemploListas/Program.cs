1d2e0a6b21dbc25f91db5ae245926eb2993189a5
hitaichou <holyfernandes@yahoo.co.uk>
2020-01-17 20:28:28 -0300

    Usado a função Insert da lista.

--- ExemploListas/Program.cs
+++ ExemploListas/Program.cs
@@ -11,10 +11,14 @@ namespace ExemploListas
             List<string> list = new List<string>();
 
             //Adição de elementos na lista
+            //O função Add adiciona o elemento no FINAL da lista.
             list.Add("Maria");
             list.Add("Alex");
             list.Add("Bob");
             list.Add("Ana");
+            //----------
+            //A função Insert insere na posição escolhida
+            list.Insert(2, "Marco");
 
             //Para cada item na lista, imprime na tela
             foreach (string obj in list)
