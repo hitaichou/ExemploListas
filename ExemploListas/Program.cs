b994872d7547422a76ca8698516b64978eb87a01
hitaichou <holyfernandes@yahoo.co.uk>
2020-01-17 20:25:26 -0300

    Instanciando Lista, adicionando item na lista e imprimindo na tela usando Foreach.

--- ExemploListas/Program.cs
+++ ExemploListas/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ExemploListas
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaração da lista
+            //Instanciação da lista vazia
+            List<string> list = new List<string>();
+
+            //Adição de elementos na lista
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Ana");
+
+            //Para cada item na lista, imprime na tela
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }            
         }
     }
 }
